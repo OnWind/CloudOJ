284579910ff398d28170aa5aa23c3604c58950af
SkyZH <iSkyZH@gmail.com>
2016-01-09 09:23:02 +0800

    minify javascript

--- build/javascript.js
+++ build/javascript.js
@@ -2,12 +2,14 @@ module.exports = function(config) {
   var gulp = require('gulp');
   var plumber = require('gulp-plumber'),
       livereload = require('gulp-livereload'),
-      gulpif = require('gulp-if');
+      gulpif = require('gulp-if'),
+      uglify = require('gulp-uglify');
   gulp.task('source:javascript', function(){
     return gulp.src([
       'src/**/*.js'
     ], { base: 'src' })
       .pipe(gulpif(!config.is_production, plumber()))
+      .pipe(gulpif(config.is_production, uglify()))
       .pipe(gulp.dest('public/'))
       .pipe(gulpif(!config.is_production, livereload()));
   });
