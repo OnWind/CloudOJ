5e38f46e4a1fdd05b17034a6d1e7bbcbb6d6ea58
SkyZH <iSkyZH@gmail.com>
2016-01-09 09:30:32 +0800

    add amd optimizer

--- build/javascript.js
+++ build/javascript.js
@@ -3,12 +3,14 @@ module.exports = function(config) {
   var plumber = require('gulp-plumber'),
       livereload = require('gulp-livereload'),
       gulpif = require('gulp-if'),
-      uglify = require('gulp-uglify');
+      uglify = require('gulp-uglify'),
+      amdOptimize = require('gulp-amd-optimizer');
   gulp.task('source:javascript', function(){
     return gulp.src([
       'src/**/*.js'
     ], { base: 'src' })
       .pipe(gulpif(!config.is_production, plumber()))
+      .pipe(gulpif(config.is_production, amdOptimize(config.amdConfig)))
       .pipe(gulpif(config.is_production, uglify()))
       .pipe(gulp.dest('public/'))
       .pipe(gulpif(!config.is_production, livereload()));
